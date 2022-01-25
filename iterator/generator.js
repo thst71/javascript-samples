c4d33f0421d7ca3c2d15cebd27169734fc19f504
Thomas Strauß <thst@beanfactory.de>
2022-01-26 00:57:21 +0100

    ein einfacher Generator

---
+++ iterator/generator.js
@@ -0,0 +1,15 @@
+function *generator() {
+    yield *[..."abcdefgh"];
+    console.log("not now, call me later");
+    yield 8;
+    console.log("yielded 8");
+    yield 0;
+    console.log("yielded 0");
+    yield 1;
+    console.log("yielded 1");
+}
+
+
+for( let x of generator()) {
+    console.log(x);
+}
