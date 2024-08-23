68e8ae69d12c42ad9ae842886735d13dd5389d2b
thst <thst@strauss-it.de>
2024-08-23 16:43:56 +0200

    added more logging

--- taggedTemplates/taggedtemplates.js
+++ taggedTemplates/taggedtemplates.js
@@ -1,5 +1,6 @@
 function magic(words, ...values) {
 
+    console.log("---");
     for(let index = 0; index < words.length; index++) {
         console.log(`${index} | "${words[index]}" | "${words.raw[index]}" | "${values[index]}"`)
     }
@@ -7,11 +8,12 @@ function magic(words, ...values) {
     return "foobar"
 }
 
-
 let medium = ['air', 'water', 'earth', 'space', 'sound', 'culture', 'tv'];
 
+console.log("before let");
 let m1 = magic`This is a \tjourney\t into ${medium[5]}!`
-let m2 = magic`This is a journey \ninto ${medium[5]}`
+let m2 = magic`This is a journey \ninto ${medium[4]}`
 
+console.log("m1, m2");
 console.log(m1, m2);
 
