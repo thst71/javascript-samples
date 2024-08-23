dd4fb5f045ca035be11ddcb9da99da3aab4240dd
thst <thst@strauss-it.de>
2024-08-23 16:52:20 +0200

    added more expressive logging and timestamps

--- web-samples/order-of-events/async-script-1.js
+++ web-samples/order-of-events/async-script-1.js
@@ -1,4 +1,4 @@
-console.log("Ich bin im async 1 - vor der Pause");
+console.log("Ich bin im async 1 - vor der Pause", new Date());
 let nowa1 = new Date().getTime();
 while (new Date().getTime() < nowa1 + 2000) {
 }
