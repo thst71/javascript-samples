6eb694f0a47e94471c92104ac586a9d46038e088
thst <thst@strauss-it.de>
2025-03-11 20:29:36 +0100

    shows example that changes the output

--- taggedTemplates/taggedtemplates.js
+++ taggedTemplates/taggedtemplates.js
@@ -12,8 +12,28 @@ let medium = ['air', 'water', 'earth', 'space', 'sound', 'culture', 'tv'];
 
 console.log("before let");
 let m1 = magic`This is a \tjourney\t into ${medium[5]}!`
-let m2 = magic`This is a journey \ninto ${medium[4]}`
+let m2 = magic`This is a
+ journey \ninto ${medium[4]}`
 
 console.log("m1, m2");
 console.log(m1, m2);
+
+
+function nospace(texts, ...values) {
+    let out = "";
+    for (let i = 0; i < texts.length; i++) {
+        out += texts[i].replaceAll(" ", "+");
+        if (i < values?.length) out += values[i];
+    }
+
+    return out;
+}
+
+console.log(nospace`space is big space is dark, it's hard to find a place to park.
+                
+                
+    oooooooooo  
+                
+                `);
+console.log(magic`space is big space is dark, it's hard to find a place to park.${1 + 1}hhh`);
 
